29764b8db14327c7137be73cc6abc02ab4c39fff
Majd Karoun <majd.karoun7@gmail.com>
2025-11-13 14:15:50 +0100

    flying bugs fixed!

--- src/components/loading-screen/LoadingScreen.jsx
+++ src/components/loading-screen/LoadingScreen.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { Howl } from 'howler';
 import './LoadingScreen.css';
 
+
 const LoadingScreen = ({ progress = 0 }) => {
   const [fakeProgress, setFakeProgress] = useState(0);
   
